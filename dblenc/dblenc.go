c900daf0732355a6946a8e17246b2bf5de68f0f6
Maciej Dobrzanski <git@dbnski.com>
2025-06-21 11:16:21 +0200

    Generate rune map from the char map

--- dblenc/dblenc.go
+++ dblenc/dblenc.go
@@ -3,6 +3,7 @@ package dblenc
 import (
     "errors"
     // "fmt"
+    "sort"
     "unicode/utf8"
 )
 
@@ -43,73 +44,6 @@ var charMap = [256]uint32{
     0x00E8, 0x00E9, 0x00EA, 0x00EB, 0x00EC, 0x00ED, 0x00EE, 0x00EF,
     0x00F0, 0x00F1, 0x00F2, 0x00F3, 0x00F4, 0x00F5, 0x00F6, 0x00F7,
     0x00F8, 0x00F9, 0x00FA, 0x00FB, 0x00FC, 0x00FD, 0x00FE, 0x00FF,
-}
-
-var encodeMap = [256]uint32{
-    0x00000000, 0x01000001, 0x02000002, 0x03000003,
-    0x04000004, 0x05000005, 0x06000006, 0x07000007,
-    0x08000008, 0x09000009, 0x0a00000a, 0x0b00000b,
-    0x0c00000c, 0x0d00000d, 0x0e00000e, 0x0f00000f,
-    0x10000010, 0x11000011, 0x12000012, 0x13000013,
-    0x14000014, 0x15000015, 0x16000016, 0x17000017,
-    0x18000018, 0x19000019, 0x1a00001a, 0x1b00001b,
-    0x1c00001c, 0x1d00001d, 0x1e00001e, 0x1f00001f,
-    0x20000020, 0x21000021, 0x22000022, 0x23000023,
-    0x24000024, 0x25000025, 0x26000026, 0x27000027,
-    0x28000028, 0x29000029, 0x2a00002a, 0x2b00002b,
-    0x2c00002c, 0x2d00002d, 0x2e00002e, 0x2f00002f,
-    0x30000030, 0x31000031, 0x32000032, 0x33000033,
-    0x34000034, 0x35000035, 0x36000036, 0x37000037,
-    0x38000038, 0x39000039, 0x3a00003a, 0x3b00003b,
-    0x3c00003c, 0x3d00003d, 0x3e00003e, 0x3f00003f,
-    0x40000040, 0x41000041, 0x42000042, 0x43000043,
-    0x44000044, 0x45000045, 0x46000046, 0x47000047,
-    0x48000048, 0x49000049, 0x4a00004a, 0x4b00004b,
-    0x4c00004c, 0x4d00004d, 0x4e00004e, 0x4f00004f,
-    0x50000050, 0x51000051, 0x52000052, 0x53000053,
-    0x54000054, 0x55000055, 0x56000056, 0x57000057,
-    0x58000058, 0x59000059, 0x5a00005a, 0x5b00005b,
-    0x5c00005c, 0x5d00005d, 0x5e00005e, 0x5f00005f,
-    0x60000060, 0x61000061, 0x62000062, 0x63000063,
-    0x64000064, 0x65000065, 0x66000066, 0x67000067,
-    0x68000068, 0x69000069, 0x6a00006a, 0x6b00006b,
-    0x6c00006c, 0x6d00006d, 0x6e00006e, 0x6f00006f,
-    0x70000070, 0x71000071, 0x72000072, 0x73000073,
-    0x74000074, 0x75000075, 0x76000076, 0x77000077,
-    0x78000078, 0x79000079, 0x7a00007a, 0x7b00007b,
-    0x7c00007c, 0x7d00007d, 0x7e00007e, 0x7f00007f,
-    0x81000081, 0x8d00008d, 0x8f00008f, 0x90000090,
-    0x9d00009d, 0xa00000a0, 0xa10000a1, 0xa20000a2,
-    0xa30000a3, 0xa40000a4, 0xa50000a5, 0xa60000a6,
-    0xa70000a7, 0xa80000a8, 0xa90000a9, 0xaa0000aa,
-    0xab0000ab, 0xac0000ac, 0xad0000ad, 0xae0000ae,
-    0xaf0000af, 0xb00000b0, 0xb10000b1, 0xb20000b2,
-    0xb30000b3, 0xb40000b4, 0xb50000b5, 0xb60000b6,
-    0xb70000b7, 0xb80000b8, 0xb90000b9, 0xba0000ba,
-    0xbb0000bb, 0xbc0000bc, 0xbd0000bd, 0xbe0000be,
-    0xbf0000bf, 0xc00000c0, 0xc10000c1, 0xc20000c2,
-    0xc30000c3, 0xc40000c4, 0xc50000c5, 0xc60000c6,
-    0xc70000c7, 0xc80000c8, 0xc90000c9, 0xca0000ca,
-    0xcb0000cb, 0xcc0000cc, 0xcd0000cd, 0xce0000ce,
-    0xcf0000cf, 0xd00000d0, 0xd10000d1, 0xd20000d2,
-    0xd30000d3, 0xd40000d4, 0xd50000d5, 0xd60000d6,
-    0xd70000d7, 0xd80000d8, 0xd90000d9, 0xda0000da,
-    0xdb0000db, 0xdc0000dc, 0xdd0000dd, 0xde0000de,
-    0xdf0000df, 0xe00000e0, 0xe10000e1, 0xe20000e2,
-    0xe30000e3, 0xe40000e4, 0xe50000e5, 0xe60000e6,
-    0xe70000e7, 0xe80000e8, 0xe90000e9, 0xea0000ea,
-    0xeb0000eb, 0xec0000ec, 0xed0000ed, 0xee0000ee,
-    0xef0000ef, 0xf00000f0, 0xf10000f1, 0xf20000f2,
-    0xf30000f3, 0xf40000f4, 0xf50000f5, 0xf60000f6,
-    0xf70000f7, 0xf80000f8, 0xf90000f9, 0xfa0000fa,
-    0xfb0000fb, 0xfc0000fc, 0xfd0000fd, 0xfe0000fe,
-    0xff0000ff, 0x8c000152, 0x9c000153, 0x8a000160,
-    0x9a000161, 0x9f000178, 0x8e00017d, 0x9e00017e,
-    0x83000192, 0x880002c6, 0x980002dc, 0x96002013,
-    0x97002014, 0x91002018, 0x92002019, 0x8200201a,
-    0x9300201c, 0x9400201d, 0x8400201e, 0x86002020,
-    0x87002021, 0x95002022, 0x85002026, 0x89002030,
-    0x8b002039, 0x9b00203a, 0x800020ac, 0x99002122,
 }
 
 type Encoding byte
@@ -170,7 +104,6 @@ func newByteMap() *byteMap {
                 ptr.Map[code] = true
             }
         }
-        // buf = buf[:0]
     }
 
     return m
@@ -243,13 +176,25 @@ func (this *byteMap) detect(data []byte) (Encoding, int) {
 }
 
 type UnDoubleEncoder struct {
-    bm *byteMap
+    byteMap *byteMap
+    runeMap []uint32
 }
 
 func NewUnDoubleEncoder() *UnDoubleEncoder {
-    return &UnDoubleEncoder{
-        bm: newByteMap(),
+    und := &UnDoubleEncoder{
+        byteMap: newByteMap(),
+        runeMap: make([]uint32, len(charMap)),
     }
+    for i := 0; i < len(charMap); i++ {
+        // Store the byte code in the upper byte and the rune value
+        // in the lower bytes.
+        und.runeMap[i] = (uint32(i) << 24) | (charMap[i] & 0x00ffffff)
+    }
+    // Sort rune map on rune value.
+    sort.Slice(und.runeMap, func(i, j int) bool {
+        return (und.runeMap[i] & 0x00ffffff) < (und.runeMap[j] & 0x00ffffff)
+    })
+    return und
 }
 
 func (this *UnDoubleEncoder) Transform(b []byte) ([]byte, error) {
@@ -261,7 +206,7 @@ func (this *UnDoubleEncoder) Transform(b []byte) ([]byte, error) {
     //     )
     // }
 
-    enc, _ := this.bm.detect(b)
+    enc, _ := this.byteMap.detect(b)
     o := b
     for enc == DOUBLE_ENCODED {
         x, err := this.transform(o)
@@ -308,7 +253,7 @@ func (this *UnDoubleEncoder) Transform(b []byte) ([]byte, error) {
         //         x, x[:], enc, at, len(x), utf8.Valid(x),
         //     )
         // }
-        enc, _ = this.bm.detect(x)
+        enc, _ = this.byteMap.detect(x)
         valid := utf8.Valid(x)
         if !valid {
             break
@@ -376,7 +321,7 @@ func (this *UnDoubleEncoder) transform(src []byte) (dst []byte, err error) {
                 return nil, ErrInvalid
             }
             mid := (low + high) / 2
-            got := encodeMap[mid]
+            got := this.runeMap[mid]
             gotRune := rune(got & (1<<24 - 1))
             if gotRune < r {
                 low = mid + 1
